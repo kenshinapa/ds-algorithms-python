fed697560d3fe201f0e0dbed2dfd520001171bd5
Arturo Padilla Ayala <r2padillaayala@gmail.com>
2022-11-19 22:43:26 -0600

    Creating recursion section and adding recursion usage notes

---
+++ 1.recursion/notes_and_examples.py
@@ -0,0 +1,20 @@
+"""I'll create examples and add notes as comments while I'm learning recursion.
+
+Notes:
+    When to use recursion:
+        - When we can easily breakdown a problem into smaller similar sub-problems.
+        - When we are fine with extra overhead (both time and space) that comes with it.
+        - When we need a quick working solution instead of efficient one.
+        - When traverse a tree or graph.
+        - When we use memoization in recursion.
+
+    When not to use recursion:
+        - If time and space complexity matters for us.
+        - Recursion uses more memory. If we use embedded memory. For example an application that
+        takes more memory in the phone is not efficient.
+        - Recursion canb be slow.
+"""
+
+if __name__ == "main":
+    pass
+  
